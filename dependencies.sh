de5f745d4d5bb6d14bf1625374d27fe3fd5a2413
Christopher Montes <chris@sapho.com>
2018-02-08 22:41:25 -0700

    added cli53 tool and and bumped version of aws-cli

--- dependencies.sh
+++ dependencies.sh
@@ -4,7 +4,7 @@ echo "installing dependencies..."
 sudo apt-get update 
 sudo apt-get install -qq -y python-pip libpython-dev
 sudo curl -O https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
-sudo pip install -q awscli==1.11.115 --upgrade # lock version
+sudo pip install -q awscli==1.14.35 --upgrade # lock version
 
 # jq json parser
 sudo apt-get install jq
@@ -12,6 +12,10 @@ sudo apt-get install jq
 # ecs-cli
 sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
 sudo chmod +x /usr/local/bin/ecs-cli
+
+# cli53
+sudo wget https://github.com/barnybug/cli53/releases/download/0.7.4/cli53-linux-amd64 -O /usr/local/bin/cli53
+sudo chmod +x /usr/local/bin/cli53
 
 # global npm
 sudo npm install -g merge-yaml-cli
